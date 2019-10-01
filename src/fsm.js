b7e22b851a9febdb101f452ff67d983bc078812d
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-10-01 10:11:56 +0300

    feat: solve task

--- src/fsm.js
+++ src/fsm.js
@@ -1,59 +1,138 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
+  /**
+   * Creates new FSM instance.
+   * @param config
+   */
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+  constructor(config) {
+    this.initial = config.initial;
+    this.states = config.states;
+    this.currentState = "normal";
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+    // for minimal history ... prev and next state;
+    this.previousState = false;
+    this.nextState = false;
+  }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+  /**
+   * Returns active state.
+   * @returns {String}
+   */
+  getState() {
+    return this.currentState;
+  }
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+  /**
+   * Goes to specified state.
+   * @param state
+   */
+  changeState(state) {
+    // take all states like array, search state in this array and
+    // if true -> start change state
+    // if false -> throw Error message
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+    if (Object.keys(this.states).indexOf(state) != -1) {
+      this.previousState = this.currentState;
+      this.currentState = state;
+      return this.currentState;
+    } else {
+      throw new Error(
+        `${state} is not existing state of Finite Student Machine`
+      );
+    }
+  }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+  /**
+   * Changes state according to event transition rules.
+   * @param event
+   */
+  trigger(event) {
+    // take current state.transitions like array, search event(trigger) in this array and
+    // if true -> start change current state and write prev state (history)
+    // if false -> throw Error message
+    if (
+      Object.keys(this.states[this.currentState].transitions).indexOf(event) !=
+      -1
+    ) {
+      this.previousState = this.currentState;
+      this.currentState = this.states[this.currentState].transitions[event];
+    } else {
+      throw new Error(
+        `${event} is not existing transition of Finite Student Machine`
+      );
+    }
+  }
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+  /**
+   * Resets FSM state to initial.
+   */
+  reset() {
+    // reset all, include history
+    this.previousState = false;
+    this.currentState = "normal";
+    this.nextState = false;
+  }
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+  /**
+   * Returns an array of states for which there are specified event transition rules.
+   * Returns all states if argument is undefined.
+   * @param event
+   * @returns {Array}
+   */
+  getStates(event) {
+    // if event send in getStates ->
+    // take array of states and filter it (search event and if find - leave it; else delete from array)
+    if (event === undefined) {
+      return Object.keys(this.states);
+    } else {
+      return Object.keys(this.states).filter(
+        value => this.states[value].transitions[event] != undefined
+      );
+    }
+  }
+
+  /**
+   * Goes back to previous state.
+   * Returns false if undo is not available.
+   * @returns {Boolean}
+   */
+  undo() {
+    // fallback with little history
+    if (this.previousState === false) {
+      return false;
+    } else {
+      this.nextState = this.currentState;
+      this.currentState = this.previousState;
+      this.previousState = false;
+      return true;
+    }
+  }
+
+  /**
+   * Goes redo to state.
+   * Returns false if redo is not available.
+   * @returns {Boolean}
+   */
+  redo() {
+    // step forward to next value (if use undo function it !== false)
+    if (this.nextState === false) {
+      return false;
+    } else {
+      this.previousState = this.currentState;
+      this.currentState = this.nextState;
+      this.nextState = false;
+      return true;
+    }
+  }
+
+  /**
+   * Clears transition history
+   */
+  clearHistory() {
+    // clear only history, not state
+    this.nextState = false;
+    this.previousState = false;
+  }
 }
 
 module.exports = FSM;
